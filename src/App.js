36c731a375fd270f4c14aa5dc725f1de2e20690f
alyonalena <elenaflikema@yandex.ru>
2025-11-18 19:21:08 +0300

    modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { Input, Tree } from 'antd'
+import './App.css'
+import { ConfigProvider, Input, Tree, Modal, Typography } from 'antd'
 import { useState, useMemo } from 'react'
 
 import Keyboard from "react-simple-keyboard"
@@ -21,6 +21,23 @@ const getParentKey = (key, tree) => {
 }
 
 function App() {
+
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+    // Perform any "OK" related actions here
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+    // Perform any "Cancel" related actions here
+  };
+
   const [layout, setLayout] = useState("default")
 
   const defaultData = [ 
@@ -90,7 +107,7 @@ function App() {
 
     const newExpandedKeys = dataList
       .map((item) => {
-        if (item.title.includes(input)) {
+        if (item.title.toLowerCase().includes(input.toLowerCase())) {
           return getParentKey(item.key, defaultData)
         }
         return null
@@ -102,90 +119,123 @@ function App() {
   }
 
   const treeData = useMemo(() => {
-    const loop = (data) =>
-      data.map((item) => {
-        const strTitle = item.title
-        const index = strTitle.indexOf(searchValue)
-        const beforeStr = strTitle.substring(0, index)
-        const afterStr = strTitle.slice(index + searchValue.length)
-        const title =
-          index > -1 ? (
-            <span key={item.key}>
-              {beforeStr}
-              <span className="site-tree-search-value">{searchValue}</span>
-              {afterStr}
-            </span>
-          ) : (
-            <span key={item.key}>{strTitle}</span>
-          )
-        if (item.children) {
-          return { title, key: item.key, children: loop(item.children) }
-        }
+      const loop = (data) =>
+        data.map((item) => {
+          const strTitle = item.title
+          const strTitleLower = item.title.toLowerCase()
+          const index = strTitle.indexOf(searchValue.toLowerCase())
+          const beforeStr = strTitle.substring(0, index)
+          const afterStr = strTitle.slice(index + searchValue.length)
+          const title =
+            index > -1 ? (
+              <span key={item.key}>
+                {beforeStr}
+                <span className="site-tree-search-value">{searchValue}</span>
+                {afterStr}
+              </span>
+            ) : (
+              <span key={item.key}>{strTitle}</span>
+            )
+          if (item.children) {
+            return { title, key: item.key, children: loop(item.children) }
+          }
 
-        return {
-          title,
-          key: item.key,
-        }
+          return {
+            title,
+            key: item.key,
+          }
       })
 
     return loop(defaultData)
   }, [searchValue])
 
   const onKeyPress = (button) => {
-    if (button === "{shift}" || button === "{lock}") {
-      setLayout(layout === "default" ? "shift" : "default")
-    }
+      if (button === "{shift}" || button === "{lock}") {
+          setLayout(layout === "default" ? "shift" : "default")
+      }
   }
 
   const greekLayout = {
-    default: [
-      "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-      "{tab} ; й ц у к е н г ш щ з х ъ [ ] \\",
-      "{lock} ф ы в а п р о л д ж э ΄ ' {enter}",
-      "{shift} я ч с м и т ь б ю , . / {shift}",
-      ".com @ {space}",
-    ],
-    shift: [
-      "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
-      "{tab} : ΅ Й Ц У К Е Н Г Ш Щ З Х Ъ { } |",
-      '{lock} Ф Ы В А П Р О Л Д Ж Э ¨ " {enter}',
-      "{shift} > Я Ч С М И Т Ь Б Ю < > ? {shift}",
-      ".com @ {space}",
-    ],
+      default: [
+          "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+          "{tab} ; й ц у к е н г ш щ з х ъ [ ] \\",
+          "{lock} ф ы в а п р о л д ж э ΄ ' {enter}",
+          "{shift} я ч с м и т ь б ю , . / {shift}",
+          ".com @ {space}",
+      ],
+      shift: [
+          "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+          "{tab} : ΅ Й Ц У К Е Н Г Ш Щ З Х Ъ { } |",
+          '{lock} Ф Ы В А П Р О Л Д Ж Э ¨ " {enter}',
+          "{shift} > Я Ч С М И Т Ь Б Ю < > ? {shift}",
+          ".com @ {space}",
+      ],
+  }
+
+  const onTreeSelect = (item) => {
+    if (item?.node?.children) {
+      return 
+    } else {
+      showModal()
+    }
   }
 
   return (
-    <div className="App-content">
-      <header className="App-header">
-        Добро пожаловать!
-      </header>
-      <div className="App-main">
-        <div className="Search-bar">
-          <Input.Search
-            value={searchValue}
-            placeholder="Поиск..."
-          />
-        </div>
-        <div className="Tree-wrapper">
-            <Tree
-                showLine
-                onExpand={onExpand}
-                expandedKeys={expandedKeys}
-                autoExpandParent={autoExpandParent}
-                treeData={treeData}
-            />
-        </div>
-      </div>
-      <div className="Keyboard-bar">
-        <Keyboard
-          layout={greekLayout}
-          layoutName={layout}
-          onChange={onChange}
-          onKeyPress={onKeyPress}
-        />
-      </div>
-    </div>
-  )
+      <ConfigProvider 
+          theme={{
+            token: {
+                "colorPrimary": "#2a1645",
+                "colorInfo": "#2a1645",
+              }
+          }}
+      >
+          <div className="App-content">
+              <header className="App-header">
+                  Добро пожаловать!
+              </header>
+              <div className="App-main">
+                  <div className="Search-bar">
+                      <Input.Search
+                        value={searchValue}
+                        placeholder="Поиск..."
+                      />
+                  </div>
+                  <div className="Tree-wrapper">
+                      <Tree
+                          showLine
+                          onExpand={onExpand}
+                          expandedKeys={expandedKeys}
+                          autoExpandParent={autoExpandParent}
+                          treeData={treeData}
+                          onSelect={(id, item) => onTreeSelect(item)}
+                      />
+                  </div>
+              </div>
+              <Modal
+                  title="Личная информация"
+                  visible={isModalVisible} // Use 'open' instead of 'visible' in Ant Design v5+
+                  onOk={handleOk}
+                  okText="Закрыть"
+                  onCancel={handleCancel}
+                  cancelButtonProps={{ style: { display: 'none' } }} // Hides the Cancel button
+              >
+                <Typography.Title>Фамилия Имя Отчество</Typography.Title>
+                <p>Должность</p>
+                <p>Опыт работы</p>
+                <p>Информация о человеке...</p>
+                <p>Что-то о его работе</p>
+              </Modal>
+              <div className="Keyboard-bar">
+                  <Keyboard
+                      layout={greekLayout}
+                      layoutName={layout}
+                      onChange={onChange}
+                      onKeyPress={onKeyPress}
+                  />
+              </div>
+          </div>
+      </ConfigProvider>
+    )
 }
 
 export default App
