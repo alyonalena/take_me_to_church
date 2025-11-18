30fffc8fde80f3d2af0f1ac53a212959b8fe5945
alyonalena <elenaflikema@yandex.ru>
2025-11-18 23:43:19 +0300

    modal details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { ConfigProvider, Input, Tree, Modal, Typography, Flex } from 'antd'
+import { Input, Tree, Modal, Typography, Flex } from 'antd'
 import { useState, useMemo, useRef } from 'react'
 
 import Keyboard from "react-simple-keyboard"
@@ -124,12 +124,13 @@ function App() {
       const loop = (data) =>
         data.map((item) => {
           const strTitle = item.title
+          let title 
           const strTitleLower = item.title.toLowerCase()
           const index = strTitleLower.indexOf(searchValue.toLowerCase())
           const beforeStr = strTitle.substring(0, index)
           const afterStr = strTitle.slice(index + searchValue.length)
           const value = strTitle.substring(index, index + searchValue.length)
-          const title =
+          title =
             index > -1 ? (
               <span key={item.key}>
                 {beforeStr}
@@ -139,6 +140,12 @@ function App() {
             ) : (
               <span key={item.key}>{strTitle}</span>
             )
+          if (item.children) {
+            title = (
+              <span style={{ fontSize: '1.1rem' }}>{title}</span>
+            )
+          }
+
           if (item.children) {
             return { title, key: item.key, children: loop(item.children) }
           }
@@ -194,77 +201,70 @@ function App() {
 
   const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\r\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\r\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".split('\r\n')
   return (
-      <ConfigProvider 
-          theme={{
-            token: {
-                "colorPrimary": "#2a1645",
-                "colorInfo": "#2a1645",
-                "": ""
-              }
-          }}
-      >
-          <div className="App-content">
-              <header className="App-header">
-                  Добро пожаловать!!!
-              </header>
-              <div className="App-main">
-                  <div className="Search-bar">
-                      <Input.Search
-                        value={searchValue}
-                        placeholder="Поиск..."
-                        allowClear={true}
-                        onClear={onClear}
-                        onChange={(e) => onChange(e.target.value)}
-                      />
-                  </div>
-                  <div className="Tree-wrapper">
-                      <Tree
-                          showLine
-                          onExpand={onExpand}
-                          expandedKeys={expandedKeys}
-                          autoExpandParent={autoExpandParent}
-                          treeData={treeData}
-                          onSelect={(id, item) => onTreeSelect(item)}
-                      />
-                  </div>
-              </div>
-              <Modal
-                  title=""
-                  visible={isModalVisible} // Use 'open' instead of 'visible' in Ant Design v5+
-                  onOk={handleOk}
-                  okText="Закрыть"
-                  onCancel={handleCancel}
-                  cancelButtonProps={{ style: { display: 'none' } }} // Hides the Cancel button
-              >
-                <Flex justify="space-between" style={{ paddingBottom: 20 }}>
+
+        <div className="App-content">
+            <header className="App-header">
+              <Typography.Title level={2} style={{ color: '#1B3041'}}>Добро пожаловать в Спассо-Парголовскую Церковь</Typography.Title>                
+            </header>
+            <div className="App-main">
+                <div className="Search-bar">
+                    <Input.Search
+                      size='large'
+                      value={searchValue}
+                      placeholder="Поиск..."
+                      allowClear={true}
+                      onClear={onClear}
+                      onChange={(e) => onChange(e.target.value)}
+                    />
+                </div>
+                <div className="Tree-wrapper">
+                    <Tree
+                      style={{ backgroundColor: '#315877'}}
+                        showLine
+                        onExpand={onExpand}
+                        expandedKeys={expandedKeys}
+                        autoExpandParent={autoExpandParent}
+                        treeData={treeData}
+                        onSelect={(id, item) => onTreeSelect(item)}
+                    />
+                </div>
+            </div>
+            <Modal
+                title=""
+                open={isModalVisible}
+                onOk={handleOk}
+                okText="Закрыть"
+                onCancel={handleCancel}
+                cancelButtonProps={{ style: { display: 'none' } }} // Hides the Cancel button
+            >
+              <Flex justify="space-between" style={{ paddingBottom: 20 }}>
                   <img
-                    draggable={false}
-                    alt="avatar"
-                    src={Img}
-                    style={{
-                        width: 200,
-                        borderRadius: 10,
-                    }}
+                      draggable={false}
+                      alt="avatar"
+                      src={Img}
+                      style={{
+                          width: 200,
+                          borderRadius: 10,
+                      }}
                   />
                   <Flex vertical justify="start" style={{ padding: 20 }}>
-                    <Typography.Title level={2}>Фамилия Имя Отчество</Typography.Title>
-                    <Typography.Text secondary> 1897 - 1945</Typography.Text>
-                    <Typography.Title level={4}>Должность</Typography.Title>
+                      <Typography.Title level={2} style={{ color: '#1B3041'}}>Фамилия Имя Отчество</Typography.Title>
+                      <Typography.Text secondary style={{ color: '#1B3041'}}> 1897 - 1945</Typography.Text>
+                      <Typography.Title level={4} style={{ color: '#1B3041'}}>Должность</Typography.Title>
                   </Flex>
-                </Flex>
-                <p>{text.map(t => <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{t}</p>)}</p>
-              </Modal>
-              <div className="Keyboard-bar">
-                  <Keyboard
-                      layout={greekLayout}
-                      keyboardRef={(r) => (keyboard.current = r)} 
-                      layoutName={layout}
-                      onChange={onChange}
-                      onKeyPress={onKeyPress}
-                  />
-              </div>
-          </div>
-      </ConfigProvider>
+              </Flex>
+              <p>{text.map(t => <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{t}</p>)}</p>
+            </Modal>
+            <div className="Keyboard-bar">
+                <Keyboard
+                    layout={greekLayout}
+                    keyboardRef={(r) => (keyboard.current = r)} 
+                    layoutName={layout}
+                    onChange={onChange}
+                    onKeyPress={onKeyPress}
+                />
+            </div>
+        </div>
     )
 }
 
